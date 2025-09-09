896705b07c513e76b3f13b771037e19532858b1f
MLNKi <96150577+MLNKi@users.noreply.github.com>
2025-09-09 02:12:12 -0300

    Add files via upload

---
+++ Controller/Scripts/crouching_player_state.gd
@@ -0,0 +1,58 @@
+class_name CrouchingPlayerState extends PlayerMovementState
+
+@export var SPEED: float = 2.0
+@export var ACCELERATION: float = 0.1
+@export var DECELERATION: float = 0.25
+@export_range(1, 6, 0.1) var CROUCH_SPEED: float = 4.0
+@export var WEAPON_BOB_SPD : float = 2.0
+@export var WEAPON_BOB_H : float = 3.0
+@export var WEAPON_BOB_V : float = 1.0
+
+@onready var CROUCH_SHAPECAST: ShapeCast3D = %CeilingDetection
+
+var RELEASED: bool = false
+
+func enter(previous_state) -> void:
+	ANIMATION.speed_scale = 1.0
+	if previous_state.name != "SlidingPlayerState":
+		ANIMATION.play("Crouch", -1.0, CROUCH_SPEED)
+	elif previous_state.name == "SlidingPlayerState":
+		ANIMATION.current_animation = "Crouch"
+		ANIMATION.seek(1.0, true)
+
+func exit() -> void:
+	RELEASED = false
+
+func update(delta):
+	PLAYER.update_gravity(delta)
+	PLAYER.update_input(SPEED, ACCELERATION, DECELERATION)
+	PLAYER.update_velocity()
+	
+	WEAPON.sway_weapon(delta, false)
+	WEAPON._weapon_bob(delta, WEAPON_BOB_SPD, WEAPON_BOB_H, WEAPON_BOB_V)
+	
+	# Handle slide when crouching while moving fast
+	if Input.is_action_just_pressed("Crouch") and PLAYER.velocity.length() > 5.0:
+		transition.emit("SlidingPlayerState")
+	
+	if Input.is_action_just_pressed("Attack"):
+		WEAPON._attack()
+		
+	# Existing crouch release logic
+	if Input.is_action_just_released("Crouch"):
+		uncrouch()
+	elif Input.is_action_pressed("Crouch") == false and RELEASED == false:
+		RELEASED = true
+		uncrouch()
+
+func uncrouch():
+	if CROUCH_SHAPECAST.is_colliding() == false:
+		ANIMATION.play("Crouch", -1.0, -CROUCH_SPEED, true)
+		await ANIMATION.animation_finished
+		if PLAYER.velocity.length() == 0:
+			transition.emit("IdlePlayerState")
+		else:
+			transition.emit("WalkingPlayerState")
+	elif CROUCH_SHAPECAST.is_colliding() == true:
+		await get_tree().create_timer(0.1).timeout
+		uncrouch()
